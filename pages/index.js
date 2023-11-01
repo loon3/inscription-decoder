8aba899a9645dd8780445005a06f3cf0133025b7
Joe Looney <hello@joelooney.org>
2023-11-01 10:32:41 -0400

    add cursed and child inscriptions

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import HexToBase64Form from '../components/convert'
-
-
+import { getInscription } from '../lib/util'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+    //FOR TESTING
+
+    // useEffect(() => {
+    //   const fetchData = async () => {
+
+    //     const txid = ""
+    //     const txUrlBlockCypher = "https://api.blockcypher.com/v1/btc/main/txs/"+txid+"?includeHex=true";
+    //     const txUrlBlockchain = "https://blockchain.info/rawtx/"+txid+"?format=hex&cors=true";
+  
+    //     const raw = await fetch(txUrlBlockCypher).then(async (response) => {
+    //       if (!response.ok) {
+    //         const secondResponse = await fetch(txUrlBlockchain);
+    //         return secondResponse.text();
+    //       }
+    //       const data = await response.json();
+    //       return data.hex;
+    //     }).catch((error) => {
+    //       throw new Error("Bad response from both servers");
+    //     });
+  
+    //     const imageData = getInscription(raw);
+    //     console.log(imageData);
+    //   };
+  
+    //   fetchData();
+    // }, []);
+  
   return (
     <div className={styles.container}>
       <Head>
